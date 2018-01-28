dfd3da1c97c19300ad077eb55b8c64ee644b47a4
Noe MAUVARIN <nmauvari@e3r8p7.42.fr>
2018-01-28 08:17:34 +0100

    modified ft_strsub

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,22 +6,38 @@
 /*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 07:00:57 by nmauvari          #+#    #+#             */
-/*   Updated: 2017/12/06 07:02:09 by nmauvari         ###   ########.fr       */
+/*   Updated: 2018/01/28 08:15:42 by nmauvari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <string.h>
+#include "libft.h"
+
+static int	check_input(char const *s, unsigned int start, size_t const len)
+{
+	size_t	new_len;
+
+	new_len = ft_strlen(s);
+	new_len -= start < new_len ? start : new_len;
+	new_len = len < new_len ? len : new_len;
+	return (new_len);
+}
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*ss;
 
-	if ((ss = malloc(len + 1)))
+	ss = 0;
+	if (s)
 	{
-		ss[len] = '\0';
-		while (len--)
-			ss[len] = s[(size_t)start + len];
+		len = check_input(s, start, len);
+		if (len && (ss = malloc(len + 1)))
+		{
+			ss[len] = '\0';
+			while (len--)
+				ss[len] = s[(size_t)start + len];
+		}
 	}
 	return (ss);
 }
