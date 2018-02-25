7c44afa28ac0bccb64747a6c325b0b9909453b0c
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-25 23:08:02 +0100

    adding a conditional to del exetution

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -21,7 +21,8 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 		while (*alst)
 		{
 			next = (*alst)->next;
-			del((*alst)->content, (*alst)->content_size);
+			if (del)
+				del((*alst)->content, (*alst)->content_size);
 			free(*alst);
 			*alst = next;
 		}
