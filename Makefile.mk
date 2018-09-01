dc3d38c9aaabe4e94692a03e6205ad6a762439f5
Noe MAUVARIN <nmauvari@e1r13p2.42.fr>
2018-09-01 04:03:07 +0200

    i

--- Makefile.mk
+++ Makefile.mk
@@ -6,30 +6,43 @@
 #    By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/12/06 07:09:48 by nmauvari          #+#    #+#              #
-#    Updated: 2018/01/28 07:36:19 by nmauvari         ###   ########.fr        #
+#    Updated: 2018/09/01 04:02:26 by nmauvari         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libft.a
 SO_NAME = libft.so
 
-OBJS =  ft_atoi.o  ft_bzero.o  ft_isalnum.o  ft_isalpha.o  ft_isascii.o\
-		ft_isdigit.o ft_isprint.o ft_itoa.o ft_lstadd.o ft_lstdel.o\
-		ft_lstdelone.o ft_lstiter.o ft_lstmap.o ft_lstnew.o ft_memalloc.o\
-		ft_memccpy.o ft_memchr.o ft_memcmp.o ft_memcpy.o ft_memdel.o\
-		ft_memmove.o ft_memset.o ft_putchar.o ft_putchar_fd.o ft_putendl.o\
-		ft_putendl_fd.o ft_putnbr.o ft_putnbr_fd.o ft_putstr.o ft_putstr_fd.o\
-		ft_strcat.o ft_strchr.o ft_strclr.o ft_strcmp.o ft_strcpy.o ft_strdel.o\
-		ft_strdup.o ft_strequ.o ft_striter.o ft_striteri.o ft_strjoin.o\
-		ft_strlcat.o ft_strlen.o ft_strmap.o ft_strmapi.o ft_strncat.o\
-		ft_strncmp.o ft_strncpy.o ft_strnequ.o ft_strnew.o ft_strnstr.o\
-		ft_strrchr.o ft_strsplit.o ft_strstr.o ft_strsub.o ft_strtrim.o\
-		ft_tolower.o ft_toupper.o
+TARGETS :=\
+		ft_atoi ft_bzero ft_isalnum ft_isalpha ft_isascii\
+		ft_isdigit ft_isprint ft_itoa ft_lstadd ft_lstdel\
+		ft_lstdelone ft_lstiter ft_lstmap ft_lstnew ft_memalloc\
+		ft_memccpy ft_memchr ft_memcmp ft_memcpy ft_memdel\
+		ft_memmove ft_memset ft_putchar ft_putchar_fd ft_putendl\
+		ft_putendl_fd ft_putnbr ft_putnbr_fd ft_putstr ft_putstr_fd\
+		ft_strcat ft_strchr ft_strclr ft_strcmp ft_strcpy ft_strdel\
+		ft_strdup ft_strequ ft_striter ft_striteri ft_strjoin\
+		ft_strlcat ft_strlen ft_strmap ft_strmapi ft_strncat\
+		ft_strncmp ft_strncpy ft_strnequ ft_strnew ft_strnstr\
+		ft_strrchr ft_strsplit ft_strstr ft_strsub ft_strtrim\
+		ft_tolower ft_toupper
 
-GCCFLAGS := -Wall -Wextra -Werror
+TARGETS_EXTENSION :=\
+	ft_atoierr ft_cleanfree
+
+TARGETS += TARGETS_EXTENSION
+
+SRCS := $(patsubst %,%.c,$(TARGETS))
+OBJS := $(patsubst %,%.o,$(TARGETS))
+
+CFLAGS :=
+DEFFLAGS := -Wall -Wextra -Werror
+SOFLAGS := -fPIC -shared
+
 
 A_STAMPS = was_liba is_liba
 SO_STAMPS = was_libso is_libso
+
 
 all: $(NAME)
 
@@ -37,8 +50,8 @@ was_libso:
 	touch was_libso
 
 is_liba: was_libso
-	touch $(patsubst %.o, %.c, $(OBJS))
-	$(eval GCCFLAGS := -Wall -Wextra -Werror)
+	touch $(SRCS)
+	$(eval CFLAGS := $(DEFFLAGS))
 
 $(NAME): is_liba $(OBJS)
 	touch $(A_STAMPS)
@@ -48,15 +61,15 @@ was_liba:
 	touch was_liba
 
 is_libso: was_liba
-	touch $(patsubst %.o, %.c, $(OBJS))
-	$(eval GCCFLAGS += -fPIC -shared)
+	touch $(SRCS)
+	$(eval CFLAGS := $(DEFFLAGS) $(SOFLAGS)
 
 so: is_libso $(OBJS)
 	touch $(SO_STAMPS)
-	gcc $(GCCFLAGS) -o $(SO_NAME) $(OBJS)
+	gcc $(CFLAGS) -o $(SO_NAME) $(OBJS)
 
 %.o: %.c
-	@gcc -c $(GCCFLAGS) $<
+	@gcc -c $(CFLAGS) $<
 
 clean:
 	-rm *.o
