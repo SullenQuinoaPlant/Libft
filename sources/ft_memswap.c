957a5a1ababf81187e733e31d49f7eb370050a3e
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-17 19:09:28 +0200

    i

--- sources/ft_memswap.c
+++ sources/ft_memswap.c
@@ -15,10 +15,19 @@
 void			ft_memswap(
 	void *a,
 	void *b,
-	void *buff,
 	size_t sz)
 {
-	ft_memcpy(buff, a, sz);
-	ft_memcpy(a, b, sz);
-	ft_memcpy(b, buff, sz);
+	void *const		lim = a + sz;
+	unsigned char	c;
+	unsigned char	*pa;
+	unsigned char	*pb;
+
+	pa = a;
+	pb = b;
+	while (a <_lim)
+	{
+		c = *a;
+		*a++ = *b;
+		*b++ = c;
+	}
 }
